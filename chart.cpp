3b58b7e2d25f02349ce3118a65b4361f31bdae7f
Alexksksss <sasha.litvak02@mail.ru>
2023-06-12 17:22:40 +0700

    Написаны файлы создания диаграмм

--- chart.cpp
+++ chart.cpp
@@ -1,1 +1,50 @@
 #include "chart.h"
+
+void BarChart::drawChart(QList<Data> data, bool isGray) {
+    // диаграмма
+    QChart* chart = getChart();
+
+    chart->setTitle("BarChart");
+
+    QBarSeries *series = new QBarSeries(chart);
+    // количество данных
+    int length = data.count();
+    for (int i = 0; i < length; i++) {
+        QBarSet* barSet = new QBarSet(data.at(i).key);
+        * barSet << data.at(i).value;
+        if (isGray)
+        {
+            int gray = (rand() % 255);
+            barSet->setBrush(QColor(gray, gray, gray));
+        }
+        else
+             barSet->setBrush(QColor(rand() % 255, rand() % 255, rand() % 255));
+        series->append(barSet);
+    }
+    chart->removeAllSeries();
+
+    chart->addSeries(series);
+    chart->createDefaultAxes();// Создает оси для диаграммы на основе ряда, который уже был добавлен в диаграмму.
+}
+
+void PieChart::drawChart(QList<Data> list, bool isGrey) {
+    // диаграмма
+    QChart* chart = getChart();
+    // заголовок
+    chart->setTitle("PieChart");
+    QPieSeries * pieSeries = new QPieSeries(chart);
+    // количество данных
+    int length = list.count();
+    for (int i = 0; i < length; i++) {
+        QPieSlice* slice = pieSeries->append(list.at(i).key, list.at(i).value);
+        if (isGrey)
+        {
+            int gray = (rand() % 255);
+            slice->setBrush(QColor(gray, gray, gray));
+        }
+        else
+            slice->setBrush(QColor(rand() % 255, rand() % 255, rand() % 255));
+    }
+    chart->removeAllSeries();
+    chart->addSeries(pieSeries);
+}
--- chart.h
+++ chart.h
@@ -1,4 +1,32 @@
 #ifndef CHART_H
 #define CHART_H
+#include <QtCharts/QChart>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QPieSeries>
+#include <QtCharts/QPieSlice>
+#include <filereader.h>
+using namespace QtCharts;
+
+class IChart {
+    QChart* chart = new QChart();
+public:
+    QChart* getChart() {return chart;}
+    virtual void drawChart(QList<Data>, bool) = 0;
+    virtual ~IChart() = default;
+};
+
+class BarChart: public IChart {
+public:
+    void drawChart(QList<Data>, bool);
+    ~BarChart() = default;
+};
+
+class PieChart: public IChart {
+public:
+    void drawChart(QList<Data>, bool);
+    ~PieChart() = default;
+};
 
 #endif // CHART_H
+
