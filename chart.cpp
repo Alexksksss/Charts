7ca594e2752695dbcc33e86128c6690a49029a91
Alexksksss <sasha.litvak02@mail.ru>
2023-06-14 13:09:41 +0700

    добавлено использование паттерна Шаблонный метод

--- chart.cpp
+++ chart.cpp
@@ -1,60 +1,91 @@
 #include "chart.h"
 
-void BarChart::drawChart(QList<Data> data, bool isGray) {
-    // количество данных
-    int length = data.count();
-    if(length==0){
-        return;
-    }
-    // диаграмма
-    QChart* chart = getChart();
-
-    chart->setTitle("BarChart");
-
-    QBarSeries *series = new QBarSeries(chart);
-
-    for (int i = 0; i < length; i++) {
-        QBarSet* barSet = new QBarSet(data.at(i).key);
-        * barSet << data.at(i).value;
-        if (isGray)
-        {
-            int gray = (rand() % 255);
-            barSet->setBrush(QColor(gray, gray, gray));
-        }
-        else
-             barSet->setBrush(QColor(rand() % 255, rand() % 255, rand() % 255));
-        series->append(barSet);
-    }
-    chart->removeAllSeries();
-
-    chart->addSeries(series);
-    chart->createDefaultAxes();// Создает оси для диаграммы на основе ряда, который уже был добавлен в диаграмму.
+IChart::IChart() {
+    chart = new QChart();
 }
 
-void PieChart::drawChart(QList<Data> list, bool isGrey) {
-    // количество данных
-    int length = list.count();
+QChart* IChart::getChart() {
+    return chart;
+}
+
+void IChart::drawChart(QList<Data> data, bool isGray) {
+    createChartTitle();
+    createChartSeries(data, isGray);
+    customizeChart();
+}
+
+void BarChart::createChartTitle() {
+    chart->setTitle("BarChart");
+}
+
+void BarChart::createChartSeries(QList<Data> data, bool isGray) {
+    int length = data.count();
+        if(length==0)
+            return;
+    QBarSeries* series = new QBarSeries(chart);
+
+    for (int i = 0; i< length; i++) {
+        QBarSet* barSet = new QBarSet(data.at(i).key);
+        *barSet << data.at(i).value;
+
+        if (isGray) {
+            int gray = (rand() % 255);
+            barSet->setBrush(QColor(gray, gray, gray));
+        } else
+            barSet->setBrush(QColor(rand() % 255, rand() % 255, rand() % 255));
+
+        series->append(barSet);
+    }
+
+//    chart->removeAllSeries();
+    removeSeries();
+      chart->addSeries(series);
+}
+
+void BarChart::customizeChart() {
+    chart->createDefaultAxes();
+}
+
+void BarChart::removeSeries(){
+    chart->removeAllSeries();
+}
+
+void PieChart::createChartTitle() {
+    chart->setTitle("PieChart");
+}
+
+
+void PieChart::createChartSeries(QList<Data> data, bool isGray) {
+    QPieSeries* pieSeries = new QPieSeries(chart);
+
+    int length = data.count();
     if (length > 10)
         length = 10;
-    if (length == 0)
+    if(length == 0)
         return;
-    // диаграмма
-    QChart* chart = getChart();
-    // заголовок
-    chart->setTitle("PieChart");
-
-    QPieSeries * pieSeries = new QPieSeries(chart);
 
     for (int i = 0; i < length; i++) {
-        QPieSlice* slice = pieSeries->append(list.at(i).key, list.at(i).value);
-        if (isGrey)
-        {
+        const Data& item = data.at(i);
+
+        QPieSlice* slice = pieSeries->append(item.key, item.value);
+
+        if (isGray) {
             int gray = (rand() % 255);
             slice->setBrush(QColor(gray, gray, gray));
-        }
-        else
+        } else {
             slice->setBrush(QColor(rand() % 255, rand() % 255, rand() % 255));
+        }
     }
-    chart->removeAllSeries();
+
+//    chart->removeAllSeries();
+    removeSeries();
     chart->addSeries(pieSeries);
+}
+
+void PieChart::customizeChart() {
+    //не нужно
+}
+
+void PieChart::removeSeries(){
+    chart->removeAllSeries();
 }
--- chart.h
+++ chart.h
@@ -6,29 +6,45 @@
 #include <QtCharts/QPieSeries>
 #include <QtCharts/QPieSlice>
 #include <filereader.h>
+
 using namespace QtCharts;
 
-//Абстрактный класс
 class IChart {
-    QChart* chart = new QChart();
+protected:
+    QChart* chart;
 public:
-    QChart* getChart() {return chart;}//геттер
-    virtual void drawChart(QList<Data>, bool) = 0;
+    IChart();
+
+    QChart* getChart();
+
+    void drawChart(QList<Data> data, bool isGray);
+
     virtual ~IChart() = default;
+
+    virtual void createChartTitle() = 0;
+    virtual void createChartSeries(QList<Data> data, bool isGray) = 0;
+    virtual void customizeChart() = 0;
+    virtual void removeSeries() = 0;
 };
 
-//Конкретные классы, переопределяющие родителя
-class BarChart: public IChart {
+class BarChart : public IChart {
 public:
-    void drawChart(QList<Data>, bool);
+    void createChartTitle() override;
+    void createChartSeries(QList<Data> data, bool isGray) override;
+    void customizeChart() override;
+    void removeSeries() override;
+
     ~BarChart() = default;
 };
 
-class PieChart: public IChart {
+class PieChart : public IChart {
 public:
-    void drawChart(QList<Data>, bool);
+    void createChartTitle() override;
+    void createChartSeries(QList<Data> data, bool isGray) override;
+    void customizeChart() override;
+    void removeSeries() override;
+
     ~PieChart() = default;
 };
 
 #endif // CHART_H
-
